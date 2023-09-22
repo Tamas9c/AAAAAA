12815b254c3e931afa92e7308099be6ac95229f4
Tamas9c <55052371+Tamas9c@users.noreply.github.com>
2023-09-22 15:12:57 +0200

    Update how_to_change_the_value_of_an_element_in_char_array.c

--- how_to_change_the_value_of_an_element_in_char_array.c
+++ how_to_change_the_value_of_an_element_in_char_array.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
-void eldontes_negy(char* a, char b, char c, char d, char e[4][1]){
-    char teljes[4][2] = {"NO", "NO", "NO", "NO"};
-    if (a == e[0]) {
-        strcpy(teljes[0], "OK");
-    }
+void eldontes_negy(char a, char b, char c, char d, char e[4]){
+    char teljes[5][3] = {"NO ", "NO ", "NO ", "NO "};
+    if (a == e[0]) strcpy(teljes[0], "OK ");
+    if (b == e[1]) strcpy(teljes[1], "OK ");
+    if (c == e[2]) strcpy(teljes[2], "OK ");
+    if (d == e[3]) strcpy(teljes[3], "OK ");
 
-
-    printf("%s %s %s %s\n", teljes[0], teljes[1], teljes[2], teljes[3]);
+    printf("%s\n", teljes[0]/*, teljes[1], teljes[2], teljes[3]*/);
 }
 
 int main() {
-
+    char bekeres_egy = 'a';
+    char bekeres_ketto = 'b';
+    char bekeres_harom = 'c';
+    char bekeres_negy = 'd';
+    char jo[4] = "abcd";
+    eldontes_negy(bekeres_egy, bekeres_ketto, bekeres_harom, bekeres_negy, jo);
     return 0;
 }
